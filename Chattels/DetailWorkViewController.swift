407d227cdc7a8128feb1b95917bc7c98c485f18b
Nishant Punia <nishantpunia3171@gmail.com>
2016-08-23 09:51:52 +0530

    Some keyboard tweaks

--- Chattels/DetailWorkViewController.swift
+++ Chattels/DetailWorkViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class DetailWorkViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+class DetailWorkViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate,UITextViewDelegate{
+    
+    //MARK: IBOutlets
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var qualityButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var writingTextView: UITextView!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    //MARK: Variables
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imagePicker = UIImagePickerController()
+        
         collectionView.delegate = self
         collectionView.dataSource = self
+        imagePicker.delegate = self
+        titleTextField.delegate = self
+        writingTextView.delegate = self
         
         self.automaticallyAdjustsScrollViewInsets = false
+        qualityButton.layer.cornerRadius = 4.0
+        qualityButton.clipsToBounds = true
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        subscribeToNotifications()
     }
     
     override func viewDidLayoutSubviews() {
@@ -30,6 +55,12 @@ class DetailWorkViewController: UIViewController,UICollectionViewDelegate,UIColl
         insets.right = 0
         self.collectionView.contentInset = insets
         self.collectionView.decelerationRate = UIScrollViewDecelerationRateFast
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeFromNotification()
     }
     
     // MARK: CollectionViewDataSource
@@ -55,6 +86,77 @@ class DetailWorkViewController: UIViewController,UICollectionViewDelegate,UIColl
         let width = CGRectGetWidth(collectionView.frame)
         let height = CGRectGetHeight(collectionView.frame)
         return CGSizeMake(width, height)
+    }
+    
+    @IBAction func pickingGalleryImage(sender: AnyObject) {
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print(pickedImage)
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func takingImageFromCamera(sender: AnyObject) {
+        imagePicker.sourceType = .Camera
+    }
+    
+    //MARK: Keyboard Notifications
+    
+    func subscribeToNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromNotification() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    func keyboardWillShow(notification: NSNotification) {
+//        if writingTextView.editable == true {
+////            view.frame.origin.y -= getKeyboardHeight(notification)
+//            writingTextView.contentInset = UIEdgeInsets(top: 8.0, left: 8.0, bottom: getKeyboardHeight(notification), right: 8.0)
+//        }
+        
+        let keyboardSize = getKeyboardHeight(notification)
+        if view.frame.origin.y == 0 {
+            self.view.frame.origin.y -= keyboardSize
+            toolbar.hidden = true
+            let range = NSMakeRange(writingTextView.text.characters.count - 1, 0)
+            writingTextView.scrollRangeToVisible(range)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y = 0
+        toolbar.hidden = false
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Text View Configuration
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            
+            return false
+        } else {
+            return true
+        }
     }
     
 }
